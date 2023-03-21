75c685806d5f4bc1bbbbdd2606313aed7b780ceb
yesl-kim <yesl00k624@gmail.com>
2023-03-22 07:29:20 +0900

    Add: 문제풀이 - k번째 큰수

--- answers/2/3. k번째 큰 수/answer.py
+++ answers/2/3. k번째 큰 수/answer.py
@@ -4,11 +4,16 @@ n = N장의 카드값 (N = len(n))
 모든 경우의 합의 숫자 중 k번째 큰 수
 '''
 
-
 # sorted: 기존 리스트를 변경하지 않고 정렬된 새로운 리스트를 반환
 # sort: 기존 리스트를 변경 후 반환하지 않음
-def solution(n,k):
-    n.sort(reverse=True)
-    return n[k]
+def solution(N,n,k):
+    a=[]
+    for i in range(N):
+        for j in range(i+1, N):
+            for f in range(j+1, N):
+                sum = n[i] + n[j] + n[f]
+                if sum not in a: a.append(sum)
+    a.sort(reverse=True) # 내림차순
+    return a[k-1]
 
-solution(list(map(int, '13 15 34 23 45 65 33 11 26 42'.split(' '))), 3)
+solution(10, list(map(int, '13 15 34 23 45 65 33 11 26 42'.split(' '))), 3)
--- answers/2/3. k번째 큰 수/judge.py
+++ answers/2/3. k번째 큰 수/judge.py
@@ -1,28 +1,27 @@
+import sys
 import answer
 
-case_num = 1
-inputPath="/Users/kimyeseul/dev/algorithm/algorithm-python/ignore/pythonalgorithm_formac/섹션 2/2. k번째 수"
-outputPath="/Users/kimyeseul/dev/algorithm/algorithm-python/ignore/pythonalgorithm_formac/섹션 2/2. k번째 수"
+case_num = 5
+path="/Users/kimyeseul/dev/algorithm/algorithm-python/ignore/pythonalgorithm_formac/섹션 2/3. k번째 큰 수"
 
 def judge():
     for i in range(case_num):
         num = i+1
-        filePath_input=inputPath + "/in" + str(num) + ".txt"
-        filePath_output=outputPath +"/out" + str(num) + ".txt"
-        input=open(filePath_input, "r").read().split('\n')
-        output=open(filePath_output, 'r').read().split('\n')
+        input_path=path + "/in" + str(num) + ".txt"
+        output_path=path +"/out" + str(num) + ".txt"
+        input=open(input_path, "r").read().split('\n')
+        output=open(output_path, 'r').read()
 
-        for c in range(int(input[0])):
-            j=c*2+1
-            [_, s, e, k] = map(int, input[j].split(' '))
-            n=list(map(int, (input[j+1].strip().split(' '))))
-            a=answer.solution(n,s,e,k)
-            if not output[c].endswith(str(a)):
+        [N, k]=list(map(int, input[0].split()))
+        n=list(map(int, input[1].split()))
+        # print(N, n, k)
+        a=answer.solution(N, n, k)
+        if not a == int(output): 
                 print('answer wrong!')
-                print('input: ', n, s, e, k)
+                print('input: ', N, n, k)
                 print('output: ', a)
-                print('expected: ', output[c])
-        else:
-            print('success!')
+                print('expected: ', output)
+    else: 
+        print('success!')
 
 judge()
