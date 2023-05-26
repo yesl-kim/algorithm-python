725849263377d8fd7e0629f01da1556a56925d1a
yesl-kim <yesl00k624@gmail.com>
2023-05-26 09:37:52 +0900

    Add: 이진탐색 구현

--- algorithms/binary-search.py
+++ algorithms/binary-search.py
@@ -1,2 +1,20 @@
 # 참고 https://www.acmicpc.net/blog/view/109
 
+def isLow(n,v): return lambda i: v<=n[i]
+
+def solution(n, m):
+    n.sort()
+    lo,hi=0,len(n)
+    check=isLow(n,m)
+    while lo+1<hi:
+        mid=int((lo+hi)/2)
+        if check(lo)==check(mid):
+            lo=mid
+        else:
+            hi=mid
+        continue
+    # lo+1=hi
+    # = lo 다음칸이 바로 hi
+    # 답은 lo or hi
+    return lo if check(lo) else hi
+
--- excercies/4. 이분검색 (결정알고리즘)&그리디알고리즘/1. 이분검색/answer.py
+++ excercies/4. 이분검색 (결정알고리즘)&그리디알고리즘/1. 이분검색/answer.py
@@ -3,24 +3,6 @@
 2. 이진탐색으로 m이 n의 몇번째인지 출력
 '''
 
-def isLow(n,v): return lambda i: v<=n[i]
-
-def solution(n, m):
-    n.sort()
-    lo,hi=0,len(n)
-    check=isLow(n,m)
-    while lo+1<hi:
-        mid=int((lo+hi)/2)
-        if check(lo)==check(mid):
-            lo=mid
-        else:
-            hi=mid
-        continue
-    # lo+1=hi
-    # = lo 다음칸이 바로 hi
-    # 답은 lo or hi
-    return lo if check(lo) else hi
-
 
 
 # print(solution(['level', 'soon', 'gooG', 'moon']))
