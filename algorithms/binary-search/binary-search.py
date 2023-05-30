4c525358a2536b70cc42c6409985aa5cd93549a8
yesl-kim <yesl00k624@gmail.com>
2023-05-31 07:48:09 +0900

    Add: 문제풀이 - 이분검색 (0.00511 s)

--- algorithms/binary-search/binary-search.py
+++ algorithms/binary-search/binary-search.py
@@ -2,8 +2,7 @@
 
 def isLow(n,v): return lambda i: v<=n[i]
 
-def solution(n, m):
-    n.sort()
+def binarySearch2(n, m):
     lo,hi=0,len(n)
     check=isLow(n,m)
     while lo+1<hi:
@@ -18,3 +17,16 @@ def solution(n, m):
     # 답은 lo or hi
     return lo if check(lo) else hi
 
+
+# 참고 | 인프런 "파이썬 알고리즘 문제풀이 입문 강의"
+def binarySearch(n, m):
+    lt,rt=0,len(n)-1
+    while lt<=rt:
+        mid=(lt+rt)//2
+        if n[mid]==m: return mid+1
+        elif m<n[mid]: rt=mid-1
+        else: lt=mid+1
+
+a=[23, 87, 65, 12, 57, 32, 99, 81]
+a.sort()
+print(binarySearch(a, 32))
--- excercies/4. 이분검색 (결정알고리즘)&그리디알고리즘/1. 이분검색/answer.py
+++ excercies/4. 이분검색 (결정알고리즘)&그리디알고리즘/1. 이분검색/answer.py
@@ -3,8 +3,18 @@
 2. 이진탐색으로 m이 n의 몇번째인지 출력
 '''
 
+# 0.00511 s
 
+def isLow(n, v): return lambda i: v <= n[i]
 
-# print(solution(['level', 'soon', 'gooG', 'moon']))
-# solution('0 1 0 0 1 0 1 1 0 0')
+def solution(n, m):
+    n.sort()
+    lo,hi=0,len(n)-1
+    check=isLow(n, m)
+    while lo+1<hi:
+        mid=int((lo+hi)/2)
+        if check(mid)==check(lo): lo=mid
+        else: hi=mid
+    return lo+1 if check(lo) else hi+1
+
 
