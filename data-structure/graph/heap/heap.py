888154ef926fe58e7468058d689327d3b7425196
yesl-kim <yesl00k624@gmail.com>
2023-06-09 17:55:47 +0900

    Add: heappop 구현

--- data-structure/graph/heap/heap.py
+++ data-structure/graph/heap/heap.py
@@ -2,6 +2,7 @@
 # data: number
 import heapq
 
+# 노드 추가
 def heappush(heap, data):
     heap.append(data)
     current=len(heap)-1
@@ -14,13 +15,34 @@ def heappush(heap, data):
             break
         
 
-h1 = [3, 4, 6, 8, 5, 7]
-h2 = [3, 4, 6, 8, 5, 7]
-heappush(h1, 2)
-heapq.heappush(h2, 2)
-print("구현한 함수의 결과: ", h1)
-print("heapq 메서드의 결과:", h2)
-heappush(h1, 3)
-heapq.heappush(h2, 3)
-print("구현한 함수의 결과: ", h1)
-print("heapq 메서드의 결과:", h2)
+# 루트 노드 제거
+def heappop(heap):
+    if not heap: return "Empty Heap!"
+    elif len(heap)==1: return heap.pop()
+    
+    root,heap[0]=heap[0],heap.pop()
+    cur,lt=0,1
+    while lt<len(heap):
+        child,rt=lt,lt+1
+        # 자식 요소 중 더 작은 값과 값교환을 하면 됨
+        if rt<len(heap) and heap[lt]>heap[rt]:
+            child=rt
+        if heap[cur]>heap[child]:
+            heap[child],heap[cur]=heap[cur],heap[child]
+            cur,lt=child,child*2+1
+        else: 
+            break
+    return root
+
+import heapq
+h = []
+arr = [21, 33, 17, 27, 9, 11, 14]
+for i in arr:
+        heappush(h, i)
+
+print(h)
+while h:
+        print(heappop(h), end = " ")
+
+print()
+print(heappop(h))
