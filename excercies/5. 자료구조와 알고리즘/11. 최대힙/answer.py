1cd584668b77e730911e002e8756fd8e297d2354
yesl-kim <yesl00k624@gmail.com>
2023-06-11 17:26:58 +0900

    Add: 문제풀이 - 최대힙

---
+++ excercies/5. 자료구조와 알고리즘/11. 최대힙/answer.py
@@ -0,0 +1,27 @@
+def max_heappush(heap, n):
+    heap.append(n)
+    cur=len(heap)-1
+    while cur>0:
+        parent=(cur-1)//2
+        if heap[cur]>heap[parent]:
+            heap[cur],heap[parent]=heap[parent],heap[cur]
+            cur=parent
+        else:
+            break
+
+def max_heappop(heap):
+    if not heap: return
+    if len(heap)==1: return heap.pop()
+
+    root,heap[0]=heap[0],heap.pop()
+    cur,lt=0,1
+    while lt<len(heap):
+        child,rt=lt,lt+1
+        if rt<len(heap) and heap[lt]<heap[rt]:
+            child=rt
+        if heap[cur]<heap[child]:
+            heap[cur],heap[child]=heap[child],heap[cur]
+            cur,lt=child,child*2+1
+        else:
+            break
+    return root
