19884e16bbe28b8cfbc45a6c65fabf28f555f953
yesl-kim <yesl00k624@gmail.com>
2023-06-13 21:07:43 +0900

    Add: 후위순회

--- excercies/6. 완전탐색/2. 이진트리 순회/answer.py
+++ excercies/6. 완전탐색/2. 이진트리 순회/answer.py
@@ -1,6 +1,7 @@
 # 이진트리 순회
 # n: 이진트리 배열
-# [1,2,3,4,5,6,7]
+
+n=[1,2,3,4,5,6,7]
 
 # 전위순회
 def preorder(n, i=0):
@@ -12,4 +13,15 @@ def preorder(n, i=0):
     if rt<l and n[rt] is not None: preorder(n, rt)
         
 
-preorder([1,2,3,4,5,6,7])
+# preorder(n)
+
+# 1. 자식요소 확인
+# 2. 형제요소 확인
+def postorder(n, i=0):
+    l=len(n)
+    lt,sibling=i*2+1, i+1
+    if lt<l and n[lt] is not None: postorder(n,lt)
+    print(n[i], end=' ')
+    if sibling<l and (sibling-1)//2 == (i-1)//2: postorder(n, sibling)
+
+postorder(n)
