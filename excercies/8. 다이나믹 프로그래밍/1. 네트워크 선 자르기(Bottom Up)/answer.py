1afbc175fefc1ca3cc76f6a197e7d1ea9d990174
yesl-kim <yesl00k624@gmail.com>
2023-07-04 09:55:35 +0900

    Add: 문제, 정답풀이 - 네트워크 선 자르기

---
+++ excercies/8. 다이나믹 프로그래밍/1. 네트워크 선 자르기(Bottom Up)/answer.py
@@ -0,0 +1,26 @@
+import sys
+
+input_path='/Users/yesl.k/Dev/coding_test/algorithm-python/ignore/섹션 8/1, 2. 네트워크 선 자르기/in2.txt'
+sys.stdin=open(input_path, 'rt')
+
+N=int(input())
+
+# dy[n]=f(n)
+dy=[0]*(N+1)
+dy[0]=0
+dy[1]=1
+dy[2]=2
+
+
+# def dfs(L=3):
+#     if L>N:
+#         return
+#     else:
+#         dy[L]=dy[L-1]+dy[L-2]
+#         dfs(L+1)
+
+# dfs()
+
+for i in range(3,N+1):
+    dy[i]=dy[i-1]+dy[i-2]
+print(dy[N])
