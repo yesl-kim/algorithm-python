597831320c3f31125de4cc60defcd0ae6ca2488e
yesl-kim <yesl00k624@gmail.com>
2023-07-12 15:39:27 +0900

    Add: 문제풀이 - closest dessert cost

---
+++ excercies/closest-dessert-cost/answer.py
@@ -0,0 +1,24 @@
+res=float('inf')
+def closestCost(baseCosts, toppingCosts, target):
+    """
+    :type baseCosts: List[int]
+    :type toppingCosts: List[int]
+    :type target: int
+    :rtype: int
+    """
+    def dfs(L,sum):
+        global res
+        a,b=abs(target-res),abs(target-sum)
+        if res<sum and a<b: return
+        if L==len(toppingCosts):
+            if a>b:
+                res=sum
+            elif a==b:
+                res=min(res,sum)
+        else:
+            for n in range(3):
+                dfs(L+1,sum+(n*toppingCosts[L]))
+        
+    for bc in baseCosts:
+        dfs(0,bc)
+    return res
