f02e02308ed8b717afd3238ac4ea2ec1fdd04496
yesl-kim <yesl00k624@gmail.com>
2023-07-26 16:11:14 +0900

    Add: 문제풀이 - 3 sum

--- 실전풀이/3sum/answer.py
+++ 실전풀이/3sum/answer.py
@@ -11,22 +11,23 @@
 #                     res.append([nums[i],nums[j],-x])
 #         return res
 
-def threeSum(nums):
-        nums.sort()
-        n,hash,res=len(nums),{},[]
-        for i in range(n-1):
-            if i and nums[i]==nums[i-1]: continue
-            for j in range(i+1,n):
-                x=nums[j]
-                c=hash.get(x)
-                if c:
-                    res.append(c+[x])
-                    del hash[x]
-                elif nums[j-1]==x:
-                    continue
-                else:
-                    hash[-(nums[i]+nums[j])]=[nums[i],nums[j]]
-        print(hash)
-        return res
+class Solution:
+     def threeSum(self, nums: List[int]) -> List[List[int]]:
+         nums.sort()
+         n,hash,res=len(nums),{},[]
+         for i in range(n-1):
+             if i and nums[i]==nums[i-1]: continue
+             for j in range(i+1,n):
+                 x=nums[j]
+                 c=hash.get(x)
+                 if c:
+                     res.append(c+[x])
+                     del hash[x]
+                 elif i+1<j and nums[j-1]==x:
+                     continue
+                 else:
+                     hash[-(nums[i]+nums[j])]=[nums[i],nums[j]]
+             hash.clear()
+         return res
 
 print(threeSum([-1,0,1,2,-1,-4]))
