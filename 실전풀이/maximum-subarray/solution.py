85a7be9996c71baa2a5bc4070c319eb818b6cee0
yesl-kim <yesl00k624@gmail.com>
2023-08-19 11:09:09 +0900

    Refactor: maximum subarray

--- 실전풀이/maximum-subarray/solution.py
+++ 실전풀이/maximum-subarray/solution.py
@@ -4,15 +4,15 @@ from itertools import accumulate
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n=len(nums)
-        if n==1: return nums[0]
-
+        if len(nums) == 1: 
+            return nums[0]
+        
         sums=list(accumulate(nums))
         min=0
         res=max(nums)
-        for i in range(n):
-            if sums[i]<min:
-                min=sums[i]
+        for x in sums:
+            if x < min:
+                min = x
             else:
-                res=max(res,sums[i],sums[i]-min)
+                res=max(res, x, x-min)
         return res
