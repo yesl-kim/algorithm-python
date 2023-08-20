76384c763048a573f4968f5c8891ea7abf4aaa11
yesl-kim <yesl00k624@gmail.com>
2023-08-20 10:12:00 +0900

    Refactor: merge-intervals - reduce 활용

--- 실전풀이/merge-intervals/solution.py
+++ 실전풀이/merge-intervals/solution.py
@@ -1,15 +1,16 @@
 # https://leetcode.com/problems/merge-intervals/
+from functools import reduce
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x:x[0])
-        
-        res = [intervals[0]]
-        for cur in intervals[1:]:
-            prev = res[-1]
+
+        def _merge(acc, cur):
+            prev = acc[-1]
             if prev[1] < cur[0]:
-                res.append(cur)
+                acc.append(cur)
             else:
                 prev[1] = max(prev[1],cur[1])
-        
-        return res
+            return acc
+
+        return reduce(_merge, intervals[1:], [intervals[0]])
