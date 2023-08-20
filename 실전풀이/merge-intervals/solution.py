d8824ed2472f27e050a9c862502434883849d525
yesl-kim <yesl00k624@gmail.com>
2023-08-20 11:13:24 +0900

    Refactor: merge intervals - 재귀 (이터레이터)

--- 실전풀이/merge-intervals/solution.py
+++ 실전풀이/merge-intervals/solution.py
@@ -5,17 +5,17 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x:x[0])
         
-        def _merge(acc, i = 1):
-            if i == len(intervals):
+        def _merge(acc, it):
+            try:
+                prev = acc[-1]
+                cur = next(it)
+                if prev[1] < cur[0]:
+                    acc.append(cur)
+                else:
+                    prev[1] = max(prev[1], cur[1])
+                return _merge(acc, it)
+            except:
                 return acc
 
-            prev = acc[-1]
-            cur = intervals[i]
-            if prev[1] < cur[0]:
-                acc.append(cur)
-            else:
-                prev[1] = max(prev[1], cur[1])
-            return _merge(acc, i+1)
-            
-            
-        return _merge([intervals[0]])
+        intervals = iter(intervals)
+        return _merge([next(intervals)], intervals)
