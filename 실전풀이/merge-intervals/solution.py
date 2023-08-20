2bf4b861a5aca2b8e8bff59c1ed78960eda55eb4
yesl-kim <yesl00k624@gmail.com>
2023-08-20 11:10:16 +0900

    Refactor: merge intervals - 재귀 사용 (인덱스)

--- 실전풀이/merge-intervals/solution.py
+++ 실전풀이/merge-intervals/solution.py
@@ -4,13 +4,18 @@ from functools import reduce
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x:x[0])
+        
+        def _merge(acc, i = 1):
+            if i == len(intervals):
+                return acc
 
-        def _merge(acc, cur):
             prev = acc[-1]
+            cur = intervals[i]
             if prev[1] < cur[0]:
                 acc.append(cur)
             else:
-                prev[1] = max(prev[1],cur[1])
-            return acc
-
-        return reduce(_merge, intervals[1:], [intervals[0]])
+                prev[1] = max(prev[1], cur[1])
+            return _merge(acc, i+1)
+            
+            
+        return _merge([intervals[0]])
