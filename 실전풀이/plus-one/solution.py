d2d45da1dbdf00a6c87580ba41cc1f3b7e4bfbf0
yesl-kim <yesl00k624@gmail.com>
2023-08-22 08:04:17 +0900

    Refactor: plus one - for else 사용, 뒤집기 먼저

--- 실전풀이/plus-one/solution.py
+++ 실전풀이/plus-one/solution.py
@@ -2,16 +2,15 @@
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n=len(digits)
-        i=n-1
-        while i<n:
-            if i<0:
-                digits.insert(0,1)
-                break
-            if digits[i]==9:
-                digits[i]=0
-                i-=1
+        digits.reverse()
+        for i, v in enumerate(digits):
+            if v == 9:
+                digits[i] = 0
             else:
-                digits[i]+=1
+                digits[i] += 1
                 break
+        else:
+            digits.append(1)
+
+        digits.reverse()
         return digits
