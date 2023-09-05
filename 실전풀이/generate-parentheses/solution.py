fabfe03cc2b3e27bd1772088fdd6ded873421c08
yesl-kim <yesl00k624@gmail.com>
2023-09-05 10:36:25 +0900

    Refactor: generate parentheses - L -> n

--- 실전풀이/generate-parentheses/solution.py
+++ 실전풀이/generate-parentheses/solution.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/generate-parentheses/
 
 from typing import List
+from functools import reduce
 
 class Solution:
     def isValid(self, s: str) -> bool:
@@ -16,29 +17,15 @@ class Solution:
 
         return len(opens) == 0
 
-
-    # def generateParenthesis(self, n: int) -> List[str]:
-    #     parentheses = ['(', ')']
-    #     res = []
-
-    #     def generate(L = 0, s = ''):
-    #         if L == n*2:
-    #             if self.isValid(s):
-    #                 res.append(s)
-    #         else:
-    #             for i in range(2):
-    #                 generate(L+1, s+parentheses[i])
-
-    #     generate()
-    #     return res
-
-    # res 제거, 반환값 활용
     def generateParenthesis(self, n: int) -> List[str]:
-        def generate(L = 0, s = ''):
-            if L == n*2:
+        pairs = ('((', '()', '))', ')(')
+        def generate(n, s = ''):
+            if n == 0:
                 return [s] if self.isValid(s) else []
             else:
-                return generate(L+1, s+'(') + generate(L+1, s+')')
+                return reduce(lambda acc, p: acc + generate(n-1, s + p), pairs, [])
 
-        return generate()
-    
+        return generate(n)
+
+s = Solution()
+print(s.generateParenthesis(3))
