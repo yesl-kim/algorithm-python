de672a3f2e6371223e1c597e4704977410eb7113
yesl-kim <yesl00k624@gmail.com>
2023-09-05 11:01:18 +0900

    Refactor: merge intervals - 빈 배열 처리

--- 실전풀이/merge-intervals/solution.py
+++ 실전풀이/merge-intervals/solution.py
@@ -5,22 +5,22 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
         def _merge(intervals):
-            if len(intervals) == 1:
+            if len(intervals) <= 1:
                 return intervals
-            else:
-                prev, cur = intervals.pop(0), intervals[0]
+            
+            next, cur = intervals.pop(), intervals[-1] # 여기가 문제
+            if cur[1] < next[0]:
+                return _merge(intervals) + [next]
+            
+            cur[0], cur[1] = min(cur[0], next[0]), next[1]
+            return _merge(intervals)
 
-                if prev[1] < cur[0]:
-                    return [prev] + _merge(intervals)
-                
-                cur[0], cur[1] = prev[0], max(prev[1], cur[1])
-                return _merge(intervals)
-
-        intervals.sort(key=lambda x:x[0])
+        intervals.sort(key=lambda x:x[1])
         return _merge(intervals)
             
     
 
 s = Solution()
-intervals = [[15,18], [1,3],[2,6],[8,10]]
+intervals = []
+# intervals = [[15,18], [1,3],[2,6],[8,10]]
 print('=>', s.merge(intervals))
