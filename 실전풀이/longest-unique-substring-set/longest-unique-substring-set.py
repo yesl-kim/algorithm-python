824375e7d714531d58f9df942a998eba742d5175
yesl-kim <yesl00k624@gmail.com>
2023-09-08 12:03:00 +0900

    Add: longest unique substring set

---
+++ 실전풀이/longest-unique-substring-set/longest-unique-substring-set.py
@@ -0,0 +1,47 @@
+# https://docs.google.com/document/d/1kI0aOmsuok64NuGV4IF8yFV0HdBdJBtQm07G01Hllr4/edit
+
+from typing import List
+from collections import defaultdict, deque
+
+def solution(s: str) -> List[str]:
+    # 범위
+    indices = defaultdict(list)
+    for i, char in enumerate(s):
+        indices[char].append(i)
+    
+    # merge
+    # TODO: 재귀로 바꿔보기
+    # 더 빠른 방법 생각해보기
+    ranges = sorted(indices.values(), key=lambda x: x[0])
+    first = ranges.pop(0) # 🤔
+    merged = [[first[0], first[-1]]]
+    for r in ranges:
+        prev = merged[-1]
+        if r[0] < prev[1]:
+            prev[1] = max(r[-1], prev[1])
+        else:
+            merged.append([r[0], r[-1]])
+
+    # 부분문자열로 반환
+    substrings = []
+    for start, end in merged:
+        substrings.append(s[start: end+1])
+    return substrings
+
+    
+
+
+inputs = ['abcbedfed', 'abcdefg', 'abmowodfsxadejihgepczpc']
+expected = [["a", "bcb", "edfed"], ['a', 'b', 'c', 'd', 'e', 'f', 'g'],  ["abmowodfsxad", "ejihge", "pczpc"]
+]
+            
+for i, input in enumerate(inputs):
+    # print(solution(input))
+    e, o = expected[i], solution(input)
+    if e != o:
+        print(f"{i+1}. wrong")
+        print(f"input: {input}")
+        print(f"output: {o}")
+        print(f"expected: {e}")
+        break
+print('success')
