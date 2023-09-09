c06c6ba91dda3879d0a37fd0a3148d4c224805c6
yesl-kim <yesl00k624@gmail.com>
2023-09-09 10:08:13 +0900

    Refactor: summary range - 마지막 요소는 반복문 밖에서

--- 실전풀이/summary-range/summary-range.py
+++ 실전풀이/summary-range/summary-range.py
@@ -1,25 +1,27 @@
 # https://leetcode.com/problems/summary-ranges/
-# 재귀?
 
 from typing import List
 
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        _range = lambda s, e: [str(nums[s]) if s == e else f"{nums[s]}->{nums[e]}"]
-        def summary(s, e):
-            if e == len(nums):
-                return _range(s, e-1)
-            
-            if nums[e]-nums[s] == e-s:
-                return summary(s, e+1)
-
-            return _range(s, e-1) + summary(e, e+1)
-        
         if not nums:
-            return nums
-        return summary(0, 1)
-    
+            return []
+        
+        n = len(nums)-1
+        left = 0
+        res = []
+
+        for i in range(n):
+            if nums[i]+1 != nums[i+1]:
+                res.append(str(nums[i]) if left == i else f"{nums[left]}->{nums[i]}")
+                left = i+1
+
+        res.append(str(nums[n]) if left == n else f"{nums[left]}->{nums[n]}")
+        
+        return res
 
 s = Solution()
-nums = [0,1,2,4,5,7]
+# nums = [0,1,2,4,5,7]
+nums = []
+# nums = [-1]
 print(s.summaryRanges(nums))
