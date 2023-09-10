9199ab4cd1155ef15fe8470c94f7db5ff1f6ec05
yesl-kim <yesl00k624@gmail.com>
2023-09-10 09:52:45 +0900

    Refactor: array partition

--- 실전풀이/array-partition/array-partition.py
+++ 실전풀이/array-partition/array-partition.py
@@ -1,8 +1,9 @@
 # https://leetcode.com/problems/array-partition
-
+from typing import List
 from functools import reduce
+
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
-        return reduce(lambda acc, i: acc + min(nums[i], nums[i+1]), range(0, len(nums), 2), 0)
+        return sum(map(min, zip(nums[::2], nums[1::2])))
