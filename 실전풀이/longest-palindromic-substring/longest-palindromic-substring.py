7158081ec7c687225fcada3597a84477f342e9a2
yesl-kim <yesl00k624@gmail.com>
2023-09-11 11:17:13 +0900

    Refactor: longest palidromic substring

--- 실전풀이/longest-palindromic-substring/longest-palindromic-substring.py
+++ 실전풀이/longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,18 +1,20 @@
 # https://leetcode.com/problems/longest-palindromic-substring
 
 class Solution:
-     def longestPalindrome(self, s: str) -> str:
-         if len(s) == 1:
-             return s
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n == 1:
+            return s
 
-         res = ''
-         for i in range(len(s)):
-             for size in range(2):
-                 left, right = i, i + size
-                 while 0 <= left and right < len(s) and s[left] == s[right]:
-                     if len(res) < right - left + 1:
-                         res = s[left: right + 1]
-                     left -= 1
-                     right += 1
+        res = ''
+        for i in range(n):
+            for size in range(2):
+                left, right = i, i + size
+                while 0 <= left and right < n and s[left] == s[right]:
+                    substring = s[left: right + 1]
+                    if len(res) < len(substring):
+                        res = substring
+                    left -= 1
+                    right += 1
 
-         return res
+        return res
