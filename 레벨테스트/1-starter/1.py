fd1e501e12651c0818a3f930996a3e2598d462ea
yesl-kim <yesl00k624@gmail.com>
2023-09-11 11:04:38 +0900

    Refactor: 공원 산책

--- 레벨테스트/1-starter/1.py
+++ 레벨테스트/1-starter/1.py
@@ -1,31 +1,35 @@
-def solution(park, routes):
-    park = [list(x) for x in park]
-    h, w = len(park), len(park[0])
 
-    for x in range(h):
-        for y in range(w):
-            if park[x][y] == 'S':
+
+def solution(park, routes):
+    H, W = len(park), len(park[0])
+    direction = {'E': (0, 1), 'S': (1, 0), 'W': (0, -1), 'N': (-1, 0)}
+
+    for i in range(H):
+        for j in range(W):
+            if park[i][j] == 'S':
+                x, y = i, j
                 break
 
+    movable = lambda x, y : 0 <= x < H and 0 <= y < W and park[x][y] != 'X'
     for r in routes:
-        direction, distance = r.split()
-        distance = int(distance)
-        
-        if direction == 'N':
-            if 0 <= x - distance < h and all([park[x - d][y] != 'X' for d in range(1, distance + 1)]):
-                x -= distance
+        _, distance = r.split()
+        dx, dy = direction[_]
 
-        if direction == 'S':
-            if 0 <= x + distance < h and all([park[x + d][y] != 'X' for d in range(1, distance + 1)]):
-                x += distance
-
-        if direction == 'W':
-            if 0 <= y - distance < w and all([park[x][y - d] != 'X' for d in range(1, distance + 1)]):
-                y -= distance
-
-        if direction == 'E':
-            if 0 <= y + distance < w and all([park[x][y + d] != 'X' for d in range(1, distance + 1)]):
-                y += distance
-
+        xx, yy = x, y
+        for _ in range(int(distance)):
+            xx += dx
+            yy += dy
+            if not movable(xx, yy):
+                break
+        else:
+            x, y = xx, yy
 
     return [x, y]
+
+park = ["OSO","OOO","OXO","OOO"]
+routes = ["E 2","S 3","W 1"]
+# park = ["SOO","OXX","OOO"]
+# routes = ["E 2","S 2","W 1"]
+# park = ["SOO","OOO","OOO"]
+# routes = ["E 2","S 2","W 1"]
+print('=>', solution(park, routes))
