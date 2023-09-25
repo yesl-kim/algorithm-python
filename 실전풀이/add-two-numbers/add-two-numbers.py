0572d8c01d1b8074df438a8911cc53c563090aa7
yesl-kim <yesl00k624@gmail.com>
2023-09-25 10:08:24 +0900

    Fix: add two numbers

--- 실전풀이/add-two-numbers/add-two-numbers.py
+++ 실전풀이/add-two-numbers/add-two-numbers.py
@@ -1,36 +1,33 @@
 # https://leetcode.com/problems/add-two-numbers
 
+# Definition for singly-linked list.
 class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
-
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution:
+    def up(self, node: ListNode):
+        if node.val < 10:
+            return
+        node.val, next_val = node.val % 10, node.val // 10
+        if node.next:
+            node.next.val += next_val
+        else:
+            node.next = ListNode(next_val)
+                
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         n1, n2 = l1, l2
         while n1 and n2:
             if not n1.next and n2.next:
                 n1.next, n2.next = n2.next, n1.next
-
-            _sum = n1.val + n2.val
-            cur_val, next_val = _sum % 10, _sum // 10
-            if _sum < 10:
-                n1.val = _sum
-            else:
-                n1.val = cur_val
-                if n1.next:
-                    n1.next.val += next_val
-                else:
-                    n1.next = ListNode(next_val)
+            
+            n1.val += n2.val
+            self.up(n1)
             n1, n2 = n1.next, n2.next
-
+        
         while n1:
-            n1.val, next_val = n1.val % 10, n1.val // 10
-            if next_val > 0:
-                if n1.next:
-                    n1.next.val += next_val
-                else:
-                    n1.next = ListNode(next_val)
+            self.up(n1)
             n1 = n1.next
-    
+        
         return l1
