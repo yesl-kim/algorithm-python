9f3843ad4739b8dbe1cbe6426f7c6162dc840a23
yesl-kim <yesl00k624@gmail.com>
2023-10-28 12:41:13 +0900

    refactor: 가장 가까운 글자

--- 실전풀이/가장 가까운 같은 글자.py
+++ 실전풀이/가장 가까운 같은 글자.py
@@ -2,11 +2,8 @@ def solution(s):
     res = []
     last_index = {}
     for i, x in enumerate(s):
-        j = last_index.get(x, -1)
-        if j < 0:
-            res.append(-1)
-        else:
-            res.append(i - j)
+        j = last_index.get(x, None)
+        res.append(i-j if j is not None else -1)
         last_index[x] = i
     return res
 
