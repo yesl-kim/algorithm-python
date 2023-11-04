02b0a65ec12004bcbcb6195a557f1f1e55f94a8d
yesl-kim <yesl00k624@gmail.com>
2023-11-04 14:32:24 +0900

    fix(1482): check 로직 수정
    - while -> for문으로 변경

--- 실전풀이/1482.minimum-number-of-days-to-make-m-bouquets.py
+++ 실전풀이/1482.minimum-number-of-days-to-make-m-bouquets.py
@@ -2,26 +2,19 @@ from typing import List
 
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        n = len(bloomDay)
         def check(x):
-            s, e = 0, 1
-            adj_flowers_cnt = []
-            bloomed = lambda i: bloomDay[i] <= x
+            bouquets = 0
+            flowers = 0
+            for day in bloomDay:
+                if day <= x:
+                    flowers += 1
+                else:
+                    bouquets += (flowers // k)
+                    flowers = 0
             
-            while s < n and e < n:
-                while s < n and not bloomed(s):
-                    s += 1
+            bouquets += (flowers // k)
+            return bouquets >= m
 
-                e = s
-                while e < n and bloomed(e):
-                    e += 1
-                
-                adj_flowers_cnt.append(e - s)
-                s = e + 1
-
-            flowers = sum(adj_flowers_cnt)
-            bouquets = sum(cnt // k for cnt in adj_flowers_cnt)
-            return flowers >= m * k and bouquets >= m
             
         # 0 < x <= max(bloomDay)
         lo, hi = 1, max(bloomDay)
@@ -36,10 +29,10 @@ class Solution:
         return res if res else -1
     
 
-m = 3
+# m = 3
 m = 1
 k = 1
 # k = 2
-bloomDay = [1, 10, 3, 10, 2]
+# bloomDay = [1, 10, 3, 10, 2]
 bloomDay = [1000000000,1000000000]
 print('=>', Solution().minDays(bloomDay, m, k))
