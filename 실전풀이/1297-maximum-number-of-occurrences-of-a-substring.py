fb7f991315b82429e608c20f11f6fcf800daee51
yesl-kim <yesl00k624@gmail.com>
2023-11-10 20:25:51 +0900

    refactor: maximum number of occurrences of a substring
    - 기존 시간복잡도 + generator 활용

--- 실전풀이/1297-maximum-number-of-occurrences-of-a-substring.py
+++ 실전풀이/1297-maximum-number-of-occurrences-of-a-substring.py
@@ -1,16 +1,17 @@
 from collections import defaultdict, Counter
 
+def window(arr, size):
+    for i in range(len(arr) - size + 1):
+        yield arr[i: i + size]
+
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
         subs_cnt = defaultdict(int)
-        
-        for i in range(len(s)):
-            for size in range(minSize, maxSize + 1):
-                if len(s) < i + size:
-                    break
-                x = s[i: i + size]
-                if len(set(x)) <= maxLetters:
-                    subs_cnt[x] += 1
+
+        for size in range(minSize, maxSize + 1):
+            for subs in window(s, size):
+                if len(set(subs)) <= maxLetters:
+                    subs_cnt[subs] += 1
         
         return max(subs_cnt.values()) if subs_cnt else 0
 
