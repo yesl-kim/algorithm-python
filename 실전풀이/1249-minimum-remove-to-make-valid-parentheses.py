b6ceb2bb33c9dcfc71b15677db20ee1e7dace79c
yesl-kim <yesl00k624@gmail.com>
2023-11-13 17:04:47 +0900

    refactor(1249)
    - 재구활용 풀이 참고
    - 비슷한 로직이 대칭을 이루도록 수정

--- 실전풀이/1249-minimum-remove-to-make-valid-parentheses.py
+++ 실전풀이/1249-minimum-remove-to-make-valid-parentheses.py
@@ -4,22 +4,47 @@ class Solution:
         oc = 0
         cc = 0
         for x in s:
-            if x == ')' and oc <= cc:
-                continue
-            
-            ns += x
-            if x == '(':
-                oc += 1
-            elif x == ')':
+            if x == ')':
+                if oc == cc:
+                    continue
                 cc += 1
+            elif x == '(':
+                oc += 1
+            ns += x
         
         s, ns = ns, ''
         oc = 0
         for x in s:
-            if x != '(':
-                ns += x
-                continue
-            if oc < cc:
-                ns += x
+            if x == '(':
+                if oc == cc:
+                    continue
                 oc += 1
+            ns += x
         return ns
+
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        def remove(s, oc):
+            try:
+                x = next(s)
+                if x == '(':
+                    ns, cc = remove(s, oc + 1)
+                    return (x + ns, cc - 1) if cc else (ns, cc)
+                elif x == ')':
+                    if oc:
+                        ns, cc = remove(s, oc - 1)
+                        return (x + ns, cc + 1)
+                    else:
+                        return remove(s, oc)
+                else:
+                    ns, cc = remove(s, oc)
+                    return (x + ns, cc)
+            except StopIteration:
+                return ('', 0)
+
+        return remove(iter(s), 0)[0]
+    
+
+s = "lee(t(c)o)de)"
+print(Solution().minRemoveToMakeValid(s))
